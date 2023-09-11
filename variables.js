dccbea910ca702a2456d894e83576cba608c3712
Miguel Angel Islas Mendez <islasmiguel737@gmail.com>
2023-09-11 11:31:19 -0600

    03 Variables

---
+++ variables.js
@@ -0,0 +1,25 @@
+//Versiones Anteriores
+var miVariable;
+
+//de ES6 en adelante
+//const y let
+
+//const declara constantes, no permite reasignacion
+//Al crearlo debe de inicializarse
+const PI = 3.14;
+//PI= 3.28;
+
+//let declara variables
+//permite reasignacion
+//se puede declarar y luego inicializar
+//JS detecta automaticamente el tipo de dato
+let firstname;
+console.log(firstname);
+firstname='Peter Parker';
+console.log(firstname)
+console.log(typeof(firstname));
+
+firstname=18;
+console.log(firstname);
+console.log(typeof(firstname));
+
